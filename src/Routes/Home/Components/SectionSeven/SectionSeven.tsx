f33bd01073d734118ed1f8d71a0669bbad19c982
hasnat123 <hasnat_45@hotmail.co.uk>
2022-12-17 00:31:07 +0000

    Parallax effect

--- src/Routes/Home/Components/SectionSeven/SectionSeven.tsx
+++ src/Routes/Home/Components/SectionSeven/SectionSeven.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
 import { CustomAnimation } from '../../../../Helpers/Helpers'
 const SectionSevenImage1 = require('./../../../../Assets/Images/SectionSevenImage1.png')
 const SectionSevenImage2 = require('./../../../../Assets/Images/SectionSevenImage2.jpeg')
 
 
 const SectionSeven = () => {
+
+  const [offsetY, setOffsetY] = useState(0);
+
+  const HandleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() =>
+  {
+    window.addEventListener('scroll', HandleScroll);
+    console.log(window.pageYOffset);
+    return () => window.removeEventListener('scroll', HandleScroll);
+  })
+
   return (
     <div className='section-seven'>
         <img src={SectionSevenImage1} alt="features" className='features'/>
-        <img src={SectionSevenImage2} alt="" />
+        <img src={SectionSevenImage2} alt="" style={window.pageYOffset < 6055 ? {transform: `translateY(0)`} : {transform: `translateY(${(offsetY - 6055) * 0.1}px)`}}/>
         <div className="strip">
           <CustomAnimation children={
             <div className="inner">
--- src/Routes/Home/Components/SectionTwo/SectionTwo.tsx
+++ src/Routes/Home/Components/SectionTwo/SectionTwo.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
 
 import { CustomAnimation } from '../../../../Helpers/Helpers'
 
@@ -6,6 +7,18 @@ import { CustomAnimation } from '../../../../Helpers/Helpers'
 const SectionTwoImage = require('./../../../../Assets/Images/SectionTwoImage.png')
 
 const SectionTwo: React.FC = () => {
+
+  const [offsetY, setOffsetY] = useState(0);
+
+  const HandleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() =>
+  {
+    window.addEventListener('scroll', HandleScroll);
+    console.log(window.pageYOffset);
+    return () => window.removeEventListener('scroll', HandleScroll);
+  })
+
   return (
     <section className='section section-two' id='section-two'>
         <div className="section-two-inner">
@@ -15,7 +28,7 @@ const SectionTwo: React.FC = () => {
               <h2>Find thousands of recipes or kick-start your own business</h2>
             </div>
           }/>
-          <img src={SectionTwoImage} alt="" />
+          <img src={SectionTwoImage} alt="" style={window.pageYOffset < 1021 ? {transform: `translateY(${-offsetY * 0.25}px)`} : {transform: `translateY(${-1020 * 0.25}px)`}}/>
         </div>
     </section>
   )
